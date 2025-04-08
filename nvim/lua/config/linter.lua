e4f1f03316d08f0d0af7afd5ab079dc14a4e0d7a
Matt Michnal <mattm3646@gmail.com>
2025-04-07 23:32:28 -0600

    adding config fixes for conform and linter - rubocop

--- nvim/lua/config/linter.lua
+++ nvim/lua/config/linter.lua
@@ -1,46 +1,46 @@
 -- nvim/lua/config/linter.lua
 return function()
-  local lint = require("lint")
+	local lint = require("lint")
 
-  -- Configure linters for specific filetypes
-  lint.linters_by_ft = {
-    ruby = { "rubocop" },      -- Use RuboCop for Ruby files
-    go = { "golangcilint" },   -- Use Golangci-lint for Go files
-    python = { "pylint" },     -- Use Pylint for Python files
-    javascript = { "eslint_d" }, -- Use ESLint for JavaScript files
-    javascriptreact = { "eslint_d" }, -- Use ESLint for JSX files
-    typescript = { "eslint_d" },
-    typescriptreact = { "eslint_d" },
-    ["javascript.playwright"] = { "eslint_d" }
-  }
+	-- Configure linters for specific filetypes
+	lint.linters_by_ft = {
+		ruby = { "rubocop" }, -- Use RuboCop for Ruby files
+		go = { "golangcilint" }, -- Use Golangci-lint for Go files
+		python = { "pylint" }, -- Use Pylint for Python files
+		javascript = { "eslint_d" }, -- Use ESLint for JavaScript files
+		javascriptreact = { "eslint_d" }, -- Use ESLint for JSX files
+		typescript = { "eslint_d" },
+		typescriptreact = { "eslint_d" },
+		["javascript.playwright"] = { "eslint_d" },
+	}
 
-  vim.diagnostic.config({
-    severity_sort = true,
-    severity = {
-      min = vim.diagnostic.severity.WARN,  -- This hides hints and info
-    }
-  })
+	vim.diagnostic.config({
+		severity_sort = true,
+		severity = {
+			min = vim.diagnostic.severity.WARN, -- This hides hints and info
+		},
+	})
 
-  local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-  vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-    group = lint_augroup,
-    callback = function()
-      lint.try_lint(nil, {
-        on_list = function(errors)
-          vim.diagnostic.setqflist({ list = errors })
-        end
-      })
-    end,
-  })
+	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		group = lint_augroup,
+		callback = function()
+			lint.try_lint(nil, {
+				on_list = function(errors)
+					vim.diagnostic.setqflist({ list = errors })
+				end,
+			})
+		end,
+	})
 
-  vim.api.nvim_create_user_command('Lint', function()
-    require('lint').try_lint()
-  end, {})
+	vim.api.nvim_create_user_command("Lint", function()
+		require("lint").try_lint()
+	end, {})
 
-  -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-  --   group = lint_augroup,
-  --   callback = function()
-  --     lint.try_lint()
-  --   end,
-  -- })
+	-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	--   group = lint_augroup,
+	--   callback = function()
+	--     lint.try_lint()
+	--   end,
+	-- })
 end
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,38 +1,41 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        ruby = { "rubocop" },
-      },
-      formatters = {
-        prettier = {
-          prepend_args = {
-            "--single-quote",
-            "--no-semi",
-            "--trailing-comma=es5",
-          },
-        },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
-    })
-  end,
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				ruby = { "rubocop" },
+			},
+			formatters = {
+				prettier = {
+					prepend_args = {
+						"--single-quote",
+						"--no-semi",
+						"--trailing-comma=es5",
+					},
+				},
+				rubocop = {
+					args = { "--server", "--auto-correct-all", "--stderr", "--force-exclusion", "--stdin", "$FILENAME" },
+				},
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 3000,
+			},
+		})
+	end,
 }
